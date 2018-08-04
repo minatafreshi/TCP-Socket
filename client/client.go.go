ec9cdec3e2fba396fb3780eead92c949aa86c663
mina tafreshi <40373294+minatafreshi@users.noreply.github.com>
2018-08-04 14:19:23 +0430

    client and server added

---
+++ client/client.go.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"bufio"
+	"time"
+	"flag"
+	"net"
+	"regexp"
+)
+
+var host = flag.string ("host" , "192.168.1.118" , "the hostname or IP to connect to ; defualts to \"local host"\.)
+var port = flag.Int ( "port" , 65534 , "the port to connect to defaults to 65534")
+
+func main()  {
+	flag.parse()
+	dest := *host + ":" +strconv.Itoa(*port)
+	fmt.Printf("connecting to %s ... \n" . dest)
+	conn.err := net.dial("TCP" , dest)
+	if err != nil {
+		if _,t := err.(*net.OpError); t{
+			fmt.Println("some problem connecting!")
+		} else {
+			fmt.Println("unknown error" + err.Error())
+		}
+		os.Exit(1)
+	}
+	go readconnection(conn)
+	for {
+		reader := bufio.NewReader(os.stdin)
+		fmt.Print("> ")
+		test, _ := reader.Readstring("\n")
+
+		conn.setWriteDeanline(time.now().Add(1*time.second))
+		_ , err := conn.write ([]byte(text))
+		if err != nil{
+			fmt.Println(" Error Writing to stream.")
+			break
+		}	
+	}
+}
+func readconnection(conn net.conn){
+	for {
+	scanner := bufio.newscanner(conn)
+		for{
+			ok := scanner.scan()
+			text := scanner.text()
+			command := handlecommands(text)
+
+			if !command {
+				fmt.Printf("\b\b ** %s \n " , text)
+			}
+			if !ok {
+				fmt.Println("Reached EOF on server connection.")
+				break
+			}
+		}
+	}
+}
+func handlecommands(text string) bool{
+	r , err :=regexp.compile("^% . *%$")
+	if err == nil {
+		if r.matchstring(text){
+			switch {
+			case text == "%quit%":
+				fmt.Println("\b\b server is leaving . hanging up!")
+				os.Exit(0)
+				
+			}
+			return true
+		}
+	}
+	return false
+}
---
+++ server/server.go.go
@@ -0,0 +1,76 @@
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"time"
+	"net"
+	"regexp"
+)
+
+var addr = flag.String("addr", "192.168.1.118", "The address to listen to; default is \"\" (all interfaces).")
+var port = flag.Int("port", 65534, "The port to listen on; default is 65534.")
+
+func main() {
+	fmt.Println("Starting server...")
+
+	src := *addr + ":" + strconv.Itoa(*port)
+	listener, _ := net.Listen("tcp", src)
+	fmt.Printf("Listening on %s.\n", src)
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Some connection error: %s\n", err)
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	remoteAddr := conn.RemoteAddr().String()
+	fmt.Println("Client connected from " + remoteAddr)
+
+	scanner := bufio.NewScanner(conn)
+
+	for {
+		ok := scanner.Scan()
+
+		handleMessage(scanner.Text(), conn)
+
+		if !ok {
+			break
+		}
+	}
+
+	fmt.Println("Client at " + remoteAddr + " disconnected.")
+}
+
+func handleMessage(message string, conn net.Conn) {
+	fmt.Println("> " + message)
+
+	if message[0] == '/' {
+		switch {
+		case message == "/time":
+			resp := "It is " + time.Now().String() + "\n"
+			fmt.Print("< " + resp)
+			conn.Write([]byte(resp))
+
+		case message == "/quit":
+			fmt.Println("Quitting.")
+			conn.Write([]byte("I'm shutting down now.\n"))
+			fmt.Println("< " + "%quit%")
+			conn.Write([]byte("%quit%\n"))
+			os.Exit(0)
+
+		default:
+			conn.Write([]byte("Unrecognized command."))
+		}
+	}
+}
